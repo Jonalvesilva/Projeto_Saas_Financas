eeec2309fd6627df510c6c6489febf23f3c2e1f3
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-11-19 15:45:57 -0300

    feat: add subscription page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import Header from "../_components/header";
+import PlansPricing from "../_components/plans-pricing";
 
 export default async function Subscription() {
   const { userId } = await auth();
@@ -7,5 +9,19 @@ export default async function Subscription() {
   if (!userId) {
     redirect("/login");
   }
-  return <></>;
+  return (
+    <>
+      <Header />
+      <section className="w-full h-screen bg-black px-4 flex flex-col min-h-[950px]">
+        <div className="flex flex-col h-full">
+          <div className="w-full flex items-center justify-between py-6 flex-col gap-y-4 lg:flex-row lg:gap-y-0">
+            <h1 className="font-bold text-2xl text-white">Assinatura</h1>
+          </div>
+          <div className="flex flex-grow justify-center lg:items-center">
+            <PlansPricing currentPlan="basic" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
